1f2a7e8767b2e9e47bbf35629743746e21a10a77
milimcpt025 <milimcpt025@student.wethinkcode.co.za>
2025-10-02 14:33:50 +0200

    commit

--- main.py
+++ main.py
@@ -2,7 +2,41 @@ from Player import Player
 from Rooms import Room
 from Enemies import Enemy
 from time import sleep
+from random import choice
+import os
 
+def printdelay(text, delay):
+    print(text)
+    sleep(delay)
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def ask(text):
+    x = input(text).lower().strip()
+    if x in ['yes', 'y']:
+        return True
+    else:
+        return False
+
+def maze():
+    directions = []
+    userswords = []
+    for i in range(5):
+        clear()
+        word = choice(['right', 'left', 'straight', 'back'])
+        print(word)
+        sleep(1.5)
+        clear()
+        directions.append(word)
+
+    for i ,name in enumerate(['first', 'second', 'third', 'fourth', 'fifth']):
+        word = input(f'{name} direction\n')
+        userswords.append(word)
+
+    if directions == userswords:
+        return True
+    return False
 
 
 def askname():
@@ -17,21 +51,33 @@ def fight(e,p1,room):
 
     while enemy.hp > 0 and p1.hp > 0:
         for i in listo:
-
             if i == 'player':
-                print(f'Enemies health: {enemy.hp * "- "}')
-                print(f'Your health: {p1.hp * "- "}')
-                command = input('Input\n').strip().lower()
+                while True:
+                    print(f'Enemies health: {enemy.hp * "- "}')
+                    print(f'Your health: {p1.hp * "- "}')
+                    command = input('Input\n').strip().lower()
 
-                if command == 'retreat':
-                    print('You flee the battle.')
-                    return True
+                    if command in ['retreat','exit','quit']:
+                        print('You flee the battle.')
+                        return True
 
-                if command == 'attack':
-                    damage = p1.attack()
-                    print(f'You do {damage} damage.')
-                    enemy.hurt(damage)
-                    sleep(2)
+                    if command == 'attack':
+                        damage = p1.attack()
+                        print(f'You do {damage} damage.')
+                        enemy.hurt(damage)
+                        sleep(2)
+                        break
+
+                    if command == 'commands':
+                        print("""----COMBAT----
+    retreat: exit the battle. (warning: The enemy will have healed if you decide to fight them again)
+    attack: attack the enemy using your equipped weapon
+    block: IN DEVELOPMENT
+    heavy attack: IN DEVELOPMENT""")
+                        continue
+
+                    else:
+                        printdelay('The pressure of battle gets to you.\nYou stand there motionless.\n(Invalid command)',3)
 
             elif enemy.hp > 0:
                 damage = enemy.attack()
@@ -69,7 +115,9 @@ take 'item': take a certain item in the room
 use 'item': use a certain item in your inventory
 equip 'weapon': equip a weapon that is in your inventory
 fight 'enemy': fight a specific enemy in the room
+exit / quit: exit game
 ----COMBAT----
+retreat: exit the battle. (warning: The enemy will have healed if you decide to fight them again)
 attack: attack the enemy using your equipped weapon
 block: IN DEVELOPMENT
 heavy attack: IN DEVELOPMENT 
@@ -92,11 +140,31 @@ heavy attack: IN DEVELOPMENT
 
                 if roomtomove in room.locked:
                     if roomtomove == 'maze':
-                        print("""You walk up to the cold grey metal gate, it towering above you.
-A single copper padlock is all that keeps it closed. Maybe there is a way of opening it""")
-
+                        printdelay("""You walk up to the cold grey metal gate, it towering above you.
+A single copper padlock is all that keeps it closed. Maybe there is a way of opening it""",2)
                     continue
+
+
+                if roomtomove == 'maze' and room.room == 'gate':
+                    printdelay("""The gate creaks eerily as you push it open, its hinges worn down by time and the environment..""",2)
+                    printdelay("""You walk along a large dirt path, eventually coming to the entrance of a maze""",2)
+                    if not ask('Would you like to enter the maze?'):
+                        printdelay('You are too scared to enter the maze and run back.',2)
+                        continue
+                    printdelay("""You enter the maze, searching for many hours to find a path that will lead you somewhere.""", 2)
+
+                if roomtomove == 'garden' and room.room == 'maze':
+                    printdelay("""You now must find the other exit to the maze.""",2)
+                    printdelay("""While you walk between the tall hedges, you hear faint voices in the wind. They say...""",2)
+                    if not maze():
+                        printdelay("""You could not understand their directions and ended up at the center of the maze again.""",1)
+                        continue
+                    print('You make it out of the maze')
+
+
                 room.changeroom(roomtomove)
+
+
 
             if command.startswith('take '):
                 item = command.replace('take ','')
@@ -107,15 +175,18 @@ A single copper padlock is all that keeps it closed. Maybe there is a way of ope
                 p1.takeitem(item)
                 room.removeitem(item)
 
-                if item == 'sword':
-                    print("""You slowly take the sword from the skeletons hand, trying your best not to disturb his peace.
-You notice two small holes engraved on his wrist bone, like he was bitten by an animal.""")
+                if item == 'sword' and room.room == 'maze':
+                    printdelay("""You slowly take the sword from the skeletons hand, trying your best not to disturb his peace.
+You notice two small holes engraved on his wrist bone, like he was bitten by an animal.
+A snake reveals itself from the hedges of the maze, hissing at you and approaching you with speed.""",3)
                     if fight('snake',p1,room):
+                        printdelay('You rest for a while after the intense battle, and try to regain your strength.',1)
+                        p1.heal('bread')
                         continue
-                    print("""The snake slithers away from your lifeless body, triumphant.
-    A magical fairy appears from thin air and revives you.
-    'How could you die to the first enemy?', it asks.
-    It flies away""")
+                    printdelay("""The snake slithers away from your lifeless body, triumphant.
+A magical fairy appears from thin air and revives you.
+'How could you die to the first enemy?', it asks.
+It flies away""",2)
                     p1.heal('health potion')
 
             if command.startswith('use '):
@@ -162,6 +233,6 @@ You notice two small holes engraved on his wrist bone, like he was bitten by an 
 
 
         except EOFError:
-            break
+            pass
 
 main()
